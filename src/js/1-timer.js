7181f2e3aa6f33a06db5097390c08f5e3b00aeb5
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-04-29 17:06:15 +0200

    commit-2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -17,6 +17,22 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
 const timer = document.querySelector('datetime-picker');
 const buttonTimer = document.querySelector('button-timer-class');
 
