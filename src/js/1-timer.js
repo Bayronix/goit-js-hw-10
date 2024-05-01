4f48cd35c87646a1cfb45430f17a5b2cc23e0efe
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-05-01 13:40:31 +0200

    commit-1-task.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,40 +1,125 @@
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const convertMs = ms => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+const ErrorModule = () => {
+  iziToast.error({
+    class: 'error-notification',
+    title: 'Error',
+    timeout: 5000,
+    titleColor: '#fff',
+    message: 'Please choose a date in the future',
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    messageColor: '#fff',
+    progressBarColor: '#B51B1B',
+    close: true,
+  });
+};
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+const enableButtonAndPicker = () => {
+  refs.buttonStart.classList.remove('is-disable');
+  refs.datePicker.disabled = false;
+};
 
-const options = {
+const updateTimerUI = () => {
+  const currentTimeData = convertMs(refs.userSelectedDate - Date.now());
+  updateUi(currentTimeData);
+};
+
+const startTimer = () => {
+  refs.buttonStart.classList.add('is-disable');
+
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+
+  intervalID = setInterval(() => {
+    const selectedDate = refs.userSelectedDate;
+    const currentTime = Date.now();
+
+    if (selectedDate > currentTime) {
+      refs.datePicker.disabled = true;
+    } else {
+      refs.datePicker.disabled = false;
+    }
+
+    const convertedData = convertMs(selectedDate - currentTime);
+
+    if (
+      convertedData.days < 0 &&
+      convertedData.hours < 0 &&
+      convertedData.minutes < 0 &&
+      convertedData.seconds < 0
+    ) {
+      enableButtonAndPicker();
+      return clearInterval(intervalID);
+    }
+
+    updateUi(convertedData);
+  }, 1000);
+};
+
+const addLeadingZero = value => String(value).padStart(2, '0');
+
+const updateUi = value => {
+  const { days, hours, minutes, seconds } = value;
+  const timerValues = {
+    days: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]'),
+  };
+
+  timerValues.days.innerText = addLeadingZero(days);
+  timerValues.hours.innerText = addLeadingZero(hours);
+  timerValues.minutes.innerText = addLeadingZero(minutes);
+  timerValues.seconds.innerText = addLeadingZero(seconds);
+};
+
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  timer: document.querySelector('.timer'),
+  datePicker: document.querySelector('#datetime-picker'),
+  userSelectedDate: undefined,
+};
+
+let intervalID;
+
+flatpickr(refs.datePicker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentTime = Date.now();
+    const selectedByUserToNumber = new Date(selectedDates[0]).getTime();
+    if (selectedByUserToNumber < currentTime) {
+      ErrorModule();
+    } else {
+      enableButtonAndPicker();
+      refs.userSelectedDate = selectedByUserToNumber;
+      updateTimerUI();
+    }
   },
-};
+});
 
-const timer = document.querySelector('datetime-picker');
-const buttonTimer = document.querySelector('button-timer-class');
+refs.buttonStart.addEventListener('click', startTimer);
 
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,18 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+const ErrorModule = () => {
+  iziToast.error({
+    class: 'error-notification',
+    title: 'Error',
+    timeout: 5000,
+    titleColor: '#fff',
+    message: 'Please choose a date in the future',
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+    messageColor: '#fff',
+    progressBarColor: '#B51B1B',
+    close: true,
+  });
+};
