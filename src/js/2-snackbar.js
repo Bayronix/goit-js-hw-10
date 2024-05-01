08cca246eb43dc2e9702d33bb14c51a671a2b014
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-05-01 19:31:16 +0200

    commit-2-snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,19 +1,63 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const ErrorModule = () => {
-  iziToast.error({
-    class: 'error-notification',
-    title: 'Error',
-    timeout: 5000,
-    titleColor: '#fff',
-    message: 'Please choose a date in the future',
-    position: 'topRight',
-    backgroundColor: '#EF4040',
-    messageColor: '#fff',
-    progressBarColor: '#B51B1B',
-    close: true,
-  });
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name=delay]'),
+  inputsState: document.querySelectorAll('input[name=state]'),
 };
+
+iziToast.settings({
+  timeout: 5000,
+  titleColor: '#fff',
+  position: 'topRight',
+  messageColor: '#fff',
+  icon: '',
+  close: false,
+});
+
+refs.form.addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+  event.preventDefault();
+
+  const delay = parseInt(refs.inputDelay.value);
+  const stateValue = getStateValue();
+
+  try {
+    if (stateValue === 'fulfilled') {
+      await delayFunction(delay);
+      showNotification('success', `✅ Fulfilled promise in ${delay}ms`);
+    } else if (stateValue === 'rejected') {
+      await delayFunction(delay);
+      throw new Error(`❌ Rejected promise in ${delay}ms`);
+    }
+  } catch (error) {
+    showNotification('error', error.message);
+  } finally {
+    refs.form.reset();
+  }
+}
+
+function getStateValue() {
+  for (const input of refs.inputsState) {
+    if (input.checked) {
+      return input.value;
+    }
+  }
+}
+
+function delayFunction(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function showNotification(type, message) {
+  const backgroundColor = type === 'success' ? '#6ED171' : '#F67474';
+  const progressBarColor = type === 'success' ? '#00BF00' : '#F00000';
+
+  iziToast[type]({
+    message: message,
+    backgroundColor: backgroundColor,
+    progressBarColor: progressBarColor,
+  });
+}
